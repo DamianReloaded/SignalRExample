34336ab81b954b38ab147c72c95f9c00964ab83f
Damian Paz <dpaz@itrio.net>
2024-03-17 06:22:20 -0300

    Add Project Files

---
+++ SignalRSample01/Hubs/UserHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample01.Hubs
+{
+	public class UserHub : Hub
+	{
+		public static int TotalViews { get; set; } = 0;
+		public static int TotalUsers { get; set; } = 0;
+
+		public override Task OnConnectedAsync()
+		{
+			TotalUsers++;
+			Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+			return base.OnConnectedAsync();
+		}
+
+		public override Task OnDisconnectedAsync(Exception? exception)
+		{
+			TotalUsers--;
+			Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+			return base.OnDisconnectedAsync(exception);
+		}
+
+		public async Task<string> NewWindowLoaded(string name)
+		{
+			TotalViews++;
+			await Clients.All.SendAsync("updateTotalViews", TotalViews); // to all
+			return $"hi {name}!";
+		}
+
+		//await Clients.Caller.SendAsync("updateTotalViews", TotalViews); // to caller
+		//await Clients.Others.SendAsync("updateTotalViews", TotalViews); // to all except caller
+		//await Clients.Client("[CLIENT A ID]").SendAsync("updateTotalViews", TotalViews); // to one client
+		//await Clients.Clients("[CLIENT A ID]","[CLIENT B ID]").SendAsync("updateTotalViews", TotalViews); // to many clients
+		//await Clients.AllExcept("[CLIENT X ID]", "[CLIENT Y ID]").SendAsync("updateTotalViews", TotalViews); // to all except the specified clients
+		//await Clients.User("user@domain.com").SendAsync("receiveMessage", "user", "message"); // to one client
+		//await Clients.Users("user@domain.com", "joe@domain.com").SendAsync("receiveMessage", "user", "message"); // to one client
+	}
+}
---
+++ SignalRSample01/wwwroot/js/usersCount.js
@@ -0,0 +1,28 @@
+﻿var connectionUserCount = new signalR.HubConnectionBuilder()
+    .configureLogging(signalR.LogLevel.Information) // None, Trace, Critical, Debug, Error, Warning 
+    .withUrl("/hubs/userCount", signalR.HttpTransportType.Websockets).build(); // .ServerSentEvents or .LongPolling
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+});
+
+function newWindowLoadedOnClient() {
+    //connectionUserCount.send("NewWindowLoaded");//send doesn't return a value
+    connectionUserCount.invoke("NewWindowLoaded", "Damian").then((value)=>console.log(value)); //invoke returns a value
+}
+
+function fulfilled() {
+    console.log("Connection to User Hub Successful");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    console.log("Connection to User Hub Rejected");
+}
+
+connectionUserCount.start().then(fulfilled, rejected);
